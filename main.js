e2fb2b187588e17d423cfa20a4da04f1aca66c26
Mohamed <mohamedibrahimahmedmahdi@gmail.com>
2024-08-01 12:07:31 +0300

    error

--- main.js
+++ main.js
@@ -13,61 +13,23 @@ let sequenceIndex = 0;
 let allInputs = document.querySelectorAll(".inputs input");
 let total = culculateProductPrice();
 let products = [];
-let delAllBtn = document.querySelector(".delete-all");
-let deletItemBtn = document.querySelector(".delet-item");
 
-// check if there is exist data in local storage
-if (window.localStorage.getItem("products")) {
-  let dataFromLocalStorage = JSON.parse(
-    window.localStorage.getItem("products")
-  );
-  // console.log(dataFromLocalStorage);
-
-  dataFromLocalStorage.forEach((item) => {
-    products.push(item);
-  });
-  // appear delete all button
-  delAllBtn.style.display = "block";
-  delAllBtn.children[0].innerHTML = products.length;
-  dataFromLocalStorage.forEach((item) => {
-    let productData = `<tr>
-                  <td>${item.id}</td>
-                  <td>${item.title}</td>
-                  <td>${item.salary}</td>
-                  <td>${item.tax}</td>
-                  <td>${item.adsense}</td>
-                  <td>${item.disco}</td>
-                  <td>${item.cat}</td>
-                  <td><span id="update-item">update</span></td>
-                  <td><span id="delet-item">delete</span></td>
-                </tr>`;
-    addedArea.innerHTML += productData;
-  });
-  console.log(dataFromLocalStorage[dataFromLocalStorage.length]);
-  sequenceIndex = dataFromLocalStorage.length;
-}
-
-// add culculate price whn input
-allInputs.forEach((input) => {
+// add culculate price whn input 
+allInputs.forEach(input=> {
   input.oninput = function () {
-    if (price.value && taxs.value && ads.value)
-      totalPriceArea.innerHTML =
-        +price.value + +taxs.value + +ads.value - discount.value;
+    if(price.value && taxs.value && ads.value)
+      totalPriceArea.innerHTML = +price.value + +taxs.value + +ads.value - discount.value;
     else {
       totalPriceArea.innerHTML = "";
     }
-  };
-});
+  } 
+})
 // add data when click button create
 creatBtn.onclick = function () {
   addProductToTable();
   allInputs.forEach((input) => (input.value = ""));
   totalPriceArea.innerHTML = "";
-  addToLocal();
-  if (products) {
-    delAllBtn.style.display = "block";
-    delAllBtn.children[0].innerHTML = products.length;
-  }
+  console.log(products);
 };
 
 // functions
@@ -75,13 +37,12 @@ creatBtn.onclick = function () {
 function culculateProductPrice() {
   return +price.value + +taxs.value + +ads.value - +discount.value;
 }
-// display data into page
 function addProductToTable() {
   let titleVal = title.value;
   let mountVal = mount.value;
-  let catVal = category.value;
-  let total = culculateProductPrice();
-  let allTotalInputs = document.querySelectorAll(".counting input");
+  let catVal = mount.value;
+  let total = culculateProductPrice()
+  let allTotalInputs = document.querySelectorAll(".counting input")
   if (document.querySelectorAll("tbody tr")) {
     sequenceIndex = document.querySelectorAll("tbody tr").length;
   } else {
@@ -101,38 +62,16 @@ function addProductToTable() {
                 </tr>`;
     addedArea.innerHTML += productData;
     let productInfo = {
-      id: i + 1,
+      id: i+1,
       title: titleVal,
       salary: total,
       tax: taxs.value,
       adsense: ads.value,
       disco: discount.value,
-      cat: catVal,
-    };
-    products.push(productInfo);
+      cat: catVal
+    }
+    products.push(productInfo)
   }
 }
 
-// add to local storage
-function addToLocal() {
-  window.localStorage.setItem("products", JSON.stringify(products));
-}
 
-// delet all logic
-delAllBtn.onclick = function () {
-  products = [];
-  window.localStorage.removeItem("products");
-  window.location.reload();
-};
-
-// handle delet item btn
-addedArea.onclick = function (e) {
-  console.log(e.target);
-  if (e.target.id === "delet-item") {
-    e.target.parentElement.parentElement.remove();
-    console.log(e.target.id);
-    products.splice(+(e.target.parentElement.parentElement.id) - 1,1)
-    addToLocal()
-  }
-  console.log(products[products.length - 1].id);
-};
