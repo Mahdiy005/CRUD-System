eb823ebb42af9bc4a2ec916fd8cca2083ec31819
Mohamed <mohamedibrahimahmedmahdi@gmail.com>
2024-08-02 10:15:21 +0300

    handle update buttons

--- main.js
+++ main.js
@@ -10,16 +10,14 @@ let total = document.querySelector(".total");
 let culculationFeilds = document.querySelectorAll(".counting input");
 let creatBtn = document.querySelector(".creat");
 let products = [];
-let delItem = document.querySelector("#delet-item")
-let updateItem = document.querySelector("#update-item")
-let delAll = document.querySelector(".delete-all")
-
-
+let delItem = document.querySelector("#delet-item");
+let updateItem = document.querySelector("#update-item");
+let delAll = document.querySelector(".delete-all");
 
 if (window.localStorage.getItem("products")) {
   products = JSON.parse(window.localStorage.getItem("products"));
-  displayData()
 }
+displayData();
 // culc price of product
 culculationFeilds.forEach((input) => {
   input.addEventListener("input", function () {
@@ -34,14 +32,14 @@ culculationFeilds.forEach((input) => {
 
 // handle data and store it into local storage
 creatBtn.onclick = function () {
-  for(let i = 0; i < +mount.value ; i++) {
+  for (let i = 0; i < +mount.value; i++) {
     let productData = {
       title: title.value,
-      price: price.value,
-      taxs: taxs.value,
-      ads: ads.value,
-      discount: discount.value,
-      cat: category.value
+      price: price.value || 0,
+      taxs: taxs.value || 0,
+      ads: ads.value || 0,
+      discount: discount.value || 0,
+      cat: category.value || "Unknown",
     };
     products.push(productData);
   }
@@ -54,7 +52,9 @@ creatBtn.onclick = function () {
   // clear inputs
   clearInputs();
 
-
+  // display delete all
+  delAll.style.display = "block";
+  delAll.children[0].innerHTML = products.length;
 };
 
 // clear all fields
@@ -72,7 +72,7 @@ function displayData() {
   addedArea.innerHTML = "";
   for (let i = 0; i < products.length; i++) {
     let productDiv = `<tr>
-                  <td>${i+1}</td>
+                  <td>${i + 1}</td>
                   <td>${products[i].title}</td>
                   <td>${products[i].price}</td>
                   <td>${products[i].taxs}</td>
@@ -81,28 +81,68 @@ function displayData() {
                   <td>${products[i].cat}</td>
                   <td><span id="update-item">update</span></td>
                   <td><span id="delet-item">delete</span></td>
-                </tr>`
-  addedArea.innerHTML += productDiv;
+                </tr>`;
+    addedArea.innerHTML += productDiv;
   }
 }
 
-
-// delete item 
+// delete item
 document.querySelector("tbody").onclick = function (e) {
-  if(e.target.id == "delet-item") {
-    let currentIndex = Array.from(document.querySelectorAll("tbody tr")).indexOf(e.target.parentElement.parentElement);
-    products = products.filter((item,index)=> {
-      return !(index === currentIndex)
+  if (e.target.id == "delet-item") {
+    let currentIndex = Array.from(
+      document.querySelectorAll("tbody tr")
+    ).indexOf(e.target.parentElement.parentElement);
+    products = products.filter((item, index) => {
+      return !(index === currentIndex);
     });
-    
-    displayData()
-  window.localStorage.setItem("products", JSON.stringify(products));
-  }
-}
 
-// delete all items 
+    displayData();
+    window.localStorage.setItem("products", JSON.stringify(products));
+  }
+};
+
+// update button
+document.querySelector("tbody").addEventListener("click", function (e) {
+  if (e.target.id == "update-item") {
+    document.querySelector(".update").style.left = "0";
+    creatBtn.style.left = "100%";
+    let currentIndex = Array.from(
+      document.querySelectorAll("tbody tr")
+    ).indexOf(e.target.parentElement.parentElement);
+    title.value = products[currentIndex].title;
+    price.value = products[currentIndex].price;
+    taxs.value = products[currentIndex].taxs;
+    ads.value = products[currentIndex].ads;
+    discount.value = products[currentIndex].discount;
+    category.value = products[currentIndex].cat;
+    total.children[0].innerHTML =
+      +price.value + +taxs.value + +ads.value - +discount.value;
+    mount.style.pointerEvents = "none";
+    mount.style.opacity = "0.6";
+    // when click update button
+    document.querySelector(".update").addEventListener("click", (e) => {
+      document.querySelector(".update").style.left = "-103%";
+      creatBtn.style.left = "0";
+      mount.style.pointerEvents = "all";
+      mount.style.opacity = "1";
+      products[currentIndex].title = title.value;
+      console.log(products[currentIndex].title);
+      products[currentIndex].price = price.value;
+      products[currentIndex].taxs = taxs.value;
+      products[currentIndex].ads = ads.value;
+      products[currentIndex].discount = discount.value;
+      products[currentIndex].cat = category.value;
+      displayData();
+      window.localStorage.setItem("products", JSON.stringify(products));
+      clearInputs();
+    });
+  }
+});
+
+// delete all items
 delAll.onclick = function () {
   products = [];
   window.localStorage.setItem("products", JSON.stringify(products));
-  displayData()
-}
+  displayData();
+  delAll.style.display = "none";
+};
